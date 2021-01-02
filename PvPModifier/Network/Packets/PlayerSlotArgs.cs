e1d9f11638ee0e5cfc036d0d309f63bb3f310506
Cy <52723076+Cyhcic@users.noreply.github.com>
2021-01-01 19:14:40 -0500

    Fix formatting

--- PvPModifier/Network/Packets/PlayerSlotArgs.cs
+++ PvPModifier/Network/Packets/PlayerSlotArgs.cs
@@ -3,23 +3,19 @@ using System;
 using System.IO;
 using System.IO.Streams;
 
-namespace PvPModifier.Network.Packets
-{
+namespace PvPModifier.Network.Packets {
 
-    public class PlayerSlotArgs : EventArgs
-    {
+    public class PlayerSlotArgs : EventArgs {
         public PvPPlayer Player;
         public byte SlotId;
         public short Stack;
         public byte Prefix;
         public short NetID;
 
-        public bool ExtractData(MemoryStream data, PvPPlayer player, out PlayerSlotArgs arg)
-        {
+        public bool ExtractData(MemoryStream data, PvPPlayer player, out PlayerSlotArgs arg) {
             data.ReadByte(); //Passes through the PlayerID data
 
-            arg = new PlayerSlotArgs
-            {
+            arg = new PlayerSlotArgs {
                 SlotId = (byte)data.ReadInt16(),
                 Player = player,
                 Stack = data.ReadInt16(),
